b1e2cef1cd9948641b8ff68d1a01def20dfc05a3
rahulram21 <rahulkannom21@gmail.com>
2024-01-12 21:45:07 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -3,27 +3,17 @@ const csv = require('csvtojson');
 const fs = require('fs');
 
 csv({
-  headers: ['id',
-  'question',
-  'option1',
-  'option2',
-  'option3',
-  'option4',
-  'answer'],
   colParser: {
     'options': (item, head, resultRow, row, colIdx) => {
       const options = ['option1','option2','option3','option4']
       .map(option => resultRow[option])
       .filter(Boolean);
 
-      //delete the original option columns from the resultRow
-      // ['option1','option2','option3','option4'].forEach(option => {
-      //   delete resultRow[option];
-      // })
       return {
-        id: resultRow.id,
-        question: resultRow.question,
-        options: options,
+        id: 'id',
+        question: 'question',
+        options: 'options',
+        answer: 'answer',
       }
       //colParser method parses the different options columns to a single array of 'options'
     },
@@ -31,14 +21,16 @@ csv({
 })
   .fromFile('Test1.csv')
   .then((JsonObj) => {
-    // const modifiedJson = JsonObj.map(obj=>{
-    //   return{
-    //     ...obj,
-    //     options: ['option1','option2','option3','option4']
-    //     .map(option => obj[option])
-    //     .filter(Boolean)
-    //   }
-    // })
-    console.log(JsonObj);
-    fs.writeFileSync('./Output.json', JSON.stringify(JsonObj, null, 2));
+    const modifiedJson = JsonObj.map(obj=>{
+      return{
+        id: obj.id,
+        question: obj.question,
+        options: ['option1','option2','option3','option4']
+        .map(option => obj[option])
+        .filter(Boolean),
+        answer: obj.answer
+      }
+    })
+    console.log(modifiedJson);
+    fs.writeFileSync('./Output.json', JSON.stringify(modifiedJson, null, 2));
   });
